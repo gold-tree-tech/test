1bb6442f424987c2cb1c70f4fb08daccee63bcd7
masaru <golden.tree9929@gmail.com>
2025-08-12 11:23:25 -0700

    bug-fix: image

--- src/components/Sections/Testimonials.tsx
+++ src/components/Sections/Testimonials.tsx
@@ -4,6 +4,7 @@ import {FC, memo, UIEventHandler, useCallback, useEffect, useMemo, useRef, useSt
 import {isApple, isMobile} from '../../config';
 import {SectionId, testimonial} from '../../data/data';
 import {Testimonial} from '../../data/dataDef';
+import Image from 'next/image';
 import useInterval from '../../hooks/useInterval';
 import useWindow from '../../hooks/useWindow';
 import QuoteIcon from '../Icon/QuoteIcon';
@@ -122,8 +123,7 @@ const Testimonial: FC<{testimonial: Testimonial; isActive: boolean}> = memo(
       )}>
       {image ? (
         <div className="relative h-14 w-14 shrink-0 sm:h-16 sm:w-16">
-          <QuoteIcon className="absolute -left-2 -top-2 h-4 w-4 stroke-black text-white" />
-          <img className="h-full w-full rounded-full" src={image} />
+          <Image alt="about-me-image" className="h-full w-full object-cover" src={image} />
         </div>
       ) : (
         <QuoteIcon className="h-5 w-5 shrink-0 text-white sm:h-8 sm:w-8" />
